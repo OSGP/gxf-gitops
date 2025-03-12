255ef918e3694cacb4142bb6b5392eae19c922be
Sander Verbruggen <sander@verbruggen.ws>
2025-03-12 16:02:48 +0100

    FDP-2849: parameterize the osgp image tag (for both platform and cucumber containers
    Signed-off-by: Sander Verbruggen <sander@verbruggen.ws>

--- charts/gxf-cucumber-tests/template-apply.sh
+++ charts/gxf-cucumber-tests/template-apply.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-helm install osgp-cucumber . -f values.yaml
+if [[ -n "$1" ]]; then
+  imageTag=$1
+else
+  imageTag="latest"
+fi
+helm install osgp-cucumber . -f values.yaml --set "imageTag=${imageTag}"
--- charts/gxf-platform/template-apply.sh
+++ charts/gxf-platform/template-apply.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-helm install osgp-platform . -f values.yaml
+if [[ -n "$1" ]]; then
+  imageTag=$1
+else
+  imageTag="latest"
+fi
+helm install osgp-platform . -f values.yaml --set "imageTag=${imageTag}"
