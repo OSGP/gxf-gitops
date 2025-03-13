bbc78d512a759c0454532dc6eee38959a0eae2c5
Jelle Hoffman <jelle.hoffman@alliander.com>
2025-03-13 14:02:04 +0100

    FDP-2849: Making de template-apply.sh scripts runnable from any location
    Signed-off-by: Jelle Hoffman jelle.hoffman@hotmail.com

--- charts/gxf-cucumber-tests/template-apply.sh
+++ charts/gxf-cucumber-tests/template-apply.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [[ -n "$1" ]]; then
   imageTag=$1
 else
   imageTag="latest"
 fi
-helm install osgp-cucumber . -f values.yaml --set "imageTag=${imageTag}"
+helm install osgp-cucumber "$DIR" -f "$DIR/values.yaml" --set "imageTag=${imageTag}"
--- charts/gxf-platform/template-apply.sh
+++ charts/gxf-platform/template-apply.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [[ -n "$1" ]]; then
   imageTag=$1
 else
   imageTag="latest"
 fi
-helm install osgp-platform . -f values.yaml --set "imageTag=${imageTag}"
+helm install osgp-platform "$DIR" -f "$DIR/values.yaml" --set "imageTag=${imageTag}"
