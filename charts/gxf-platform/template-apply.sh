35f7018f8f5af8101598a59279879a4869d5e093
Sander Verbruggen <sander.verbruggen@alliander.com>
2025-03-20 13:31:32 +0100

    FDP-2849: More generic setup for the template-apply scripts
    Signed-off-by: Sander Verbruggen <sander.verbruggen@alliander.com>

--- charts/gxf-platform/template-apply.sh
+++ charts/gxf-platform/template-apply.sh
@@ -2,9 +2,4 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [[ -n "$1" ]]; then
-  imageTag=$1
-else
-  imageTag="latest"
-fi
-helm install osgp-platform "$DIR" -f "$DIR/values.yaml" --set "imageTag=${imageTag}"
+../_template_apply.sh $DIR osgp-platform $*
